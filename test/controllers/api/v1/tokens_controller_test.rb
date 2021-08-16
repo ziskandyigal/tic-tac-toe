c5985132ece1f6df3637674716a71cfcd4e12363
Yigal Ziskand <yigal.ziskand@cloudinary.com>
2021-08-16 18:00:27 +0000

    appied jwt in token controller action

---
+++ test/controllers/api/v1/tokens_controller_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class Api::V1::TokensControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+  
+  test "should get jwt" do
+    post api_v1_tokens_url, params: {
+      user: {
+        email: @user.email,
+        password: "!qazsdfre$$"
+      }
+    }, as: :json
+
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_not_nil json_response['token']
+  end
+
+  test "should not get jwt" do
+    post api_v1_tokens_url, params: {
+      user: {
+        email: @user.email,
+        password: "badPass"
+      }
+    }, as: :json
+    assert_response :unauthorized
+  end
+end
