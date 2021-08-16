6c39ca1ee782d7157ec6555fc7027e19b5223027
Yigal Ziskand <yigal.ziskand@cloudinary.com>
2021-08-16 16:57:51 +0000

    adopt bcrypt on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
+
+    # utilise ActiveModel:SecurePassword:has_secure_password interface integration with bcrypt
+    has_secure_password
 end
