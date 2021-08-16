b5424ce9b45ba6380fa3f3984a79696086794e9e
Yigal Ziskand <yigal.ziskand@cloudinary.com>
2021-08-16 16:31:24 +0000

    add api versioning

--- app/javascript/app.svelte
+++ app/javascript/app.svelte
@@ -1,11 +1,7 @@
 <script>
- 
 </script>
 
 <style>
-  h1 {
-    color: #FF3E00;
-  }
 </style>
 
 <h1>Hello</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # UI
   get 'home/index'
   root 'home#index'
+
+  # API
+  namespace :apim defaults: { format: :json } do
+    namespace :v1 do
+
+    end
+  end
 end
